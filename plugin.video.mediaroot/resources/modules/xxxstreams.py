4003c9af20329d0f58131250fbbabfe169d2abd6
schmidthans <schmidt.hans.80@gmx.de>
2016-07-22 12:59:45 +0200

    xxxstreams fixed

--- plugin.video.mediaroot/resources/modules/xxxstreams.py
+++ plugin.video.mediaroot/resources/modules/xxxstreams.py
@@ -13,7 +13,7 @@ net.set_user_agent('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100
 def CATEGORIES():
     main.addDir('Search','none','xxxstreamsSearch',artwork + '/main/search.png')
     main.addDir('Newest Videos',base_url +'/new-porn-streaming/page/0','xxxstreamsIndex',artwork + '/main/recentvideos.png')
-    main.addDir('Full Movies',base_url +'/full-movies-streaming/page/0','xxxstreamsIndex',artwork + '/main/recentvideos.png')
+    main.addDir('Full Movies',base_url +'/full-porn-movie-stream/page/0','xxxstreamsIndex',artwork + '/main/recentvideos.png')
     main.addDir('HD',base_url +'/tag/hd/page/0','xxxstreamsIndex',artwork + '/main/recentvideos.png')
     link = net.http_GET(base_url).content
     match1=re.compile('menu-item menu-item-type-(taxonomy|custom).*?<a href=[\'|"](http://xxxstreams.org.+?)[\'|"]\s*>(.+?)<', re.S).findall(link)
@@ -62,7 +62,7 @@ def VIDEOLINKS(name,url,thumb):
     link = net.http_GET(url).content
     match=re.compile('<div class="entry-content">(.+?)class="navigation post-navigation"', re.S).findall(link)
     if match:
-        match1=re.compile('href="(http[s]?://[^www.pix|^www.theteen].*?)["<\s].*? target="_blank">([^<]+?)<', re.S).findall(match[0].replace('/tttr/1out2.php?s=0,100,100,100:*&amp;u=',''))
+        match1=re.compile('href="(http[s]?://[^www.pix|^www.theteen|^sharenxs]?.*?)["<\s].*?target="_blank">([^<]+?)<', re.S).findall(match[0].replace('/tttr/1out2.php?s=0,100,100,100:*&amp;u=',''))
         for url,hoster in match1:
             hoster1 = re.match('http[s]?://(.+?)/', hoster)
             if hoster1:
